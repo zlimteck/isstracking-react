72d731ccdb04ffcfe0697a6b2becd95bd4c7ad41
zlimteck <zlimteck@gmail.com>
2023-01-04 03:34:13 +0100

    Modify expedition/id fetch from api and fix details

--- src/components/ExpeditionData/ExpeditionData.jsx
+++ src/components/ExpeditionData/ExpeditionData.jsx
@@ -1,17 +1,21 @@
 import './ExpeditionData.css';
 
-function ExpeditionData ({expeditions, title, patch, date, people, ship}) {
+function ExpeditionData ({title, patch, datestart, datend, shipstarting, shipreturning}) {
     return (
         <section className="expedition_infos">
             <h2 className="expedition_title">{title}</h2>
             <img className="expedition_patch" src={patch} alt="Expedition Patch"/>
             <div className="expedition_date">
-                <h3 className="expedition_date_title">Date de l'expedition:</h3>
-                <p className="expedition_date_text">{date}</p>
+                <h3 className="expedition_date_title">Date de lancement:</h3>
+                <p className="expedition_date_text">{datestart}</p>
+                <h3 className="expedition_date_title">Date de retour:</h3>
+                <p className="expedition_date_text">{datend}</p>
             </div>
             <div className="expedition_ship">
-                <h3 className="expedition_ship_title">Navette d'arrivé et de départ: </h3>
-                <p className="expedition_ship_title">{ship}</p>
+                <h3 className="expedition_ship_title">Navette(s) d'arrivé: </h3>
+                <p className="expedition_ship_text">{shipstarting}</p>
+                <h3 className="expedition_ship_title">Navette(s) de depart: </h3>
+                <p className="expedition_ship_text">{shipreturning}</p>
             </div>
         </section>
     )
--- src/pages/Expedition/Expedition.jsx
+++ src/pages/Expedition/Expedition.jsx
@@ -1,56 +1,83 @@
-import ExpeditionsInfos from '../../data/expeditions.json';
+import axios from 'axios';
 import Error404 from '../Error/Error';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import ExpeditionData from '../../components/ExpeditionData/ExpeditionData';
 import Collapse from '../../components/Collapse/Collapse';
 import './Expedition.css';
 
-
 function Expeditions () {
     document.cookie = "SameSite=None; Secure";
-    const params = useParams(); //
-    const expeditions = 
-    ExpeditionsInfos.find((expeditions) => params.id === expeditions.id); //
-    if (expeditions===undefined) {
-        return (
-            <Error404 />
-        )
+    const params = useParams();
+    const [expedition, setExpedition] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setIsError(false);
+        axios.get(`https://api.isstracking.xyz/v1/expeditions/${params.id}`)
+        .then((response) => {
+            setExpedition(response.data);
+        })
+        .catch((error) => {
+            setIsError(true);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        });
+    }, [params.id]);
+
+    if (isError) {
+        return <Error404 />;
     }
+    if (isLoading) {
+        return <div className="loading">Chargement des données...</div>;
+    }
+    
     const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
     "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
     ];
-    const date_start = new Date(expeditions.date_start);
-    const date_end = new Date(expeditions.date_end);
-    expeditions.date_start_fr = date_start.getDate() + " " + monthNames[date_start.getMonth()] + " " + date_start.getFullYear();
-    expeditions.date_end_fr = date_end.getDate() + " " + monthNames[date_end.getMonth()] + " " + date_end.getFullYear();
-    const key = expeditions.number_expedition;
-    const name = expeditions.name_expedition;
-    const patch = expeditions.patch_expedition;
-    const date = expeditions.date_start_fr + " - " + expeditions.date_end_fr;
-    const people = expeditions.astronauts;
-    const ship = expeditions.ship_starting + " - " + expeditions.ship_return;
-    const picture = expeditions.picture_crew;
+    //recupere les dates dans le tableau et les transforme en dates en français les tableau peuvent contenir plusieurs dates
+    expedition.date_start_fr = expedition.date_start.map((date) => {
+        const dateStart = new Date(date);
+        return dateStart.getDate() + " " + monthNames[dateStart.getMonth()] + " " + dateStart.getFullYear();
+    });
+    expedition.date_end_fr = expedition.date_end.map((date) => {
+        const dateEnd = new Date(date);
+        return dateEnd.getDate() + " " + monthNames[dateEnd.getMonth()] + " " + dateEnd.getFullYear();
+    });
+    const key = expedition.number_expedition;
+    const name = expedition.name_expedition;
+    const patch = expedition.patch_expedition;
+    const datestart = expedition.date_start_fr.join(" et ");
+    const datend = expedition.date_end_fr.join(" et ");
+    const people = expedition.astronauts;
+    const shipstarting = expedition.ship_starting.join(" - ");
+    const shipreturning = expedition.ship_return.join(" - ");
+    const picture = expedition.picture_crew;
     return (
         <section className="expedition">
             <div className="expeditions_infos">
-                <ExpeditionData 
+                <ExpeditionData
                 key={key} 
-                expeditions={expeditions} 
-                title={name} 
+                expeditions={expedition} 
+                title={name}
                 patch={patch} 
-                date={date}
-                people={people} 
-                ship={ship}
+                datestart={datestart}
+                datend={datend}
+                people={people}
+                shipstarting={shipstarting}
+                shipreturning={shipreturning}
                 picture={picture}
                 />
             </div>
             <section className="collapse_astronautes">
                 <Collapse
                 title="Astronautes a bord"
-                //Cherche le tableau astronauts dans le fichier json
                 texte={people.map((astronauts, list) => (
                     <li key={list} className="list_astronauts">{astronauts}</li>
-                    ))}
+                ))}
                 />
             </section>
             <section className="collapse_crew">
