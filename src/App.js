a251944447e1bb5b92d15d482bdb1f941f08a7a5
zlimteck <zlimteck@gmail.com>
2023-01-07 17:11:27 +0100

    WIP test vercel analytics

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import Error from "./pages/Error/Error";
 import Expedition from "./pages/Expedition/Expedition";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import { inject } from '@vercel/analytics';
+
+inject()
 
 function App() {
     return (
         <React.StrictMode>
             <div className="App">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/tracking" element={<Tracking />} />
-                <Route path="/expeditions" element={<Expeditions />} />
-                <Route path="expeditions/:id" element={<Expedition />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-            <Footer />
-        </div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/tracking" element={<Tracking />} />
+                    <Route path="/expeditions" element={<Expeditions />} />
+                    <Route path="expeditions/:id" element={<Expedition />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </div>
         </React.StrictMode>
     );
 }
