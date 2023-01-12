0240bb5f15a9d9d7eb479ce3ddbe1e41306d136e
zlimteck <zlimteck@gmail.com>
2023-01-12 17:35:33 +0100

    Add Last Expedition view

---
+++ src/components/LastExpedition/LastExpedition.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import './LastExpedition.css'
+
+function LastExpedition() {
+    const [lastexpedition, setExpedition] = useState({}); //Création d'un state pour l'expédition
+
+    useEffect(() => {
+      axios
+        .get('https://api.isstracking.xyz/v1/expeditions')
+        .then(response => {
+          const lastExpedition = response.data[response.data.length - 1]; //Récupère la dernière expédition
+          setExpedition(lastExpedition);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []); //[] permet de ne pas relancer la requête à chaque fois que le state change
+
+    //Convertie les dates en français
+    const dateStart = new Date(lastexpedition.date_start);
+    const dateEnd = new Date(lastexpedition.date_end);
+    const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    lastexpedition.date_start_fr = dateStart.getDate() + " " + monthNames[dateStart.getMonth()] + " " + dateStart.getFullYear();
+    lastexpedition.date_end_fr = dateEnd.getDate() + " " + monthNames[dateEnd.getMonth()] + " " + dateEnd.getFullYear();
+
+    //Affiche chargement avant d'avoir les données
+    if (!lastexpedition.name_expedition) {
+        return <div className="Last_expedition_loading">Chargement des données...</div>
+    }
+  
+    return (
+        <div className="Last_expedtion_container">
+            <div className="Last_expedition_img">
+                <img className="Last_expedtion_patch" src={lastexpedition.patch_expedition} alt="Expedition Patch"/>
+            </div>
+            <div className="Last_expedition_text">
+                <Link className="Last_expedition_link" to={`/expeditions/${lastexpedition.number_expedition}`}>
+                    <h2 className="Last_expedition_title">{lastexpedition.name_expedition}</h2>
+                </Link>
+                <p className="Last_expedition_date"><strong>Date de début:</strong> {lastexpedition.date_start_fr}</p>
+                <p className="Last_expedition_date"><strong>Date de fin:</strong> {lastexpedition.date_end_fr}</p>
+            </div>
+        </div>
+    );
+  }
+  
+  export default LastExpedition;
+  
+  
+  
+  
+  
--- src/pages/Expeditions/Expeditions.jsx
+++ src/pages/Expeditions/Expeditions.jsx
@@ -1,9 +1,15 @@
 import './Expeditions.css';
 import Card from '../../components/Card/Card';
+import LastExpedition from '../../components/LastExpedition/LastExpedition';
+import Collapse from '../../components/Collapse/Collapse';
 
 function Missions() {
     return (
         <div className="Expeditions">
+            <Collapse
+                title={`Expedition en cours`}
+                texte= {<LastExpedition />}
+            />
             <Card />
         </div>
     );
