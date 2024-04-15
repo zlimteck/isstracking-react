bbbe309417042262fe911b32f1c6e52714972cac
zmt <zmt@MBP-zmt.local>
2024-04-16 01:23:18 +0200

    add link to my profil github in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <footer className='footer'>
             <div className='footer_content'>
                 <p>© 2022-2023 - ISS Tracking | Hébergé via <a className='footer_link' href="https://vercel.com/home" target="_blank" rel="noreferrer">VERCEL</a></p>
-                <p>@Zlimteck</p>
+                <p><a className='footer_link' href="https://github.com/zlimteck" target="_blank" rel="noreferrer">@Zlimteck</a></p>
             </div>
         </footer>
     );
