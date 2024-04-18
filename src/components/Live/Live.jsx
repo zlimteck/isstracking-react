8fef52e46702d4abe916ee1e675dedc8b040399f
zmt <zmt@MBP-zmt.local>
2024-04-18 19:33:13 +0200

    add live iss youtube source

---
+++ src/components/Live/Live.jsx
@@ -0,0 +1,18 @@
+//Function to display Live youtube video of the ISS
+import React from 'react';
+import './Live.css';
+
+function Live() {
+    return (
+        <section className="Live">
+            <h2>Live de l'ISS:</h2>
+            <section className="Live_container">
+                <iframe className='FirstLive' src="https://www.youtube.com/embed/P9C25Un7xaM" title="YouTube video player live 1" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe className='TwoLive' src="https://www.youtube.com/embed/jPTD2gnZFUw" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </section>
+        </section>
+    );
+}
+
+export default Live;
+
