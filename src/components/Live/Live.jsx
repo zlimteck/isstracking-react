2a82a2ceac12d9270832af3dd9a34af09a5297fc
zmt <zmt@MBP-zmt.local>
2024-04-21 21:22:40 +0200

    WIP Page load performance optimization for youtube videos

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -1,18 +1,40 @@
 //Function to display Live youtube video of the ISS
-import React from 'react';
+import React, { useState } from 'react';
 import './Live.css';
 
 function Live() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleIntersection = (entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
+            setIsVisible(true);
+        }
+    };
+
+    React.useEffect(() => {
+        const observer = new IntersectionObserver(handleIntersection, {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1,
+        });
+
+        observer.observe(document.querySelector('.Live'));
+
+        return () => observer.disconnect();
+    }, []);
+
     return (
         <section className="Live">
             <h2 className="title_live">Live de l'ISS:</h2>
-            <section className="Live_container">
-                <iframe className='FirstLive' src="https://www.youtube.com/embed/P9C25Un7xaM" title="YouTube video player live 1" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <iframe className='TwoLive' src="https://www.youtube.com/embed/jPTD2gnZFUw" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </section>
+            {isVisible && (
+                <section className="Live_container">
+                    <iframe className='FirstLive' src="https://www.youtube.com/embed/P9C25Un7xaM" title="YouTube video player live 1" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe className='TwoLive' src="https://www.youtube.com/embed/jPTD2gnZFUw" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </section>
+            )}
         </section>
     );
 }
 
 export default Live;
-
