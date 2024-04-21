66ff820b8ddc9fae448ecf833344196e9ae8f694
zmt <zmt@MBP-zmt.local>
2024-04-21 17:50:55 +0200

    fix err on h2

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -5,7 +5,7 @@ import './Live.css';
 function Live() {
     return (
         <section className="Live">
-            <h2>Live de l'ISS:</h2>
+            <h2 className="title_live">Live de l'ISS:</h2>
             <section className="Live_container">
                 <iframe className='FirstLive' src="https://www.youtube.com/embed/P9C25Un7xaM" title="YouTube video player live 1" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <iframe className='TwoLive' src="https://www.youtube.com/embed/jPTD2gnZFUw" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
