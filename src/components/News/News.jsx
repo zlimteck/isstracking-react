a26f56c3468f5e96944dd4c10640f6ef2fff3e64
zmt <zmt@MBP-zmt.local>
2024-04-25 11:51:10 +0200

    Update performances

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,4 +1,3 @@
-//Fonction pour afficher les daily report de l'iss
 import React, { useEffect, useState } from 'react';
 import './News.css';
 import axios from 'axios';
@@ -11,53 +10,30 @@ const images = [
 ];
 
 function News() {
-    const [isVisible, setIsVisible] = useState(false);
     const [news, setNews] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://api.spaceflightnewsapi.net/v4/reports?limit=6');
-                console.log('Response from API:', response.data);
+                const response = await axios.get('https://api.spaceflightnewsapi.net/v4/reports?limit=3');
                 if (Array.isArray(response.data.results)) {
                     setNews(response.data.results);
-                } else {
-                    console.error('Results array not found in API response:', response.data);
                 }
             } catch (error) {
                 console.error('Error fetching data: ', error);
             }
         };
 
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
-                setIsVisible(true);
-                fetchData();
-                observer.disconnect();
-            }
-        }, {
-            root: null,
-            rootMargin: '0px',
-            threshold: 1,
-        });
-
-        observer.observe(document.querySelector('.News'));
-
-        return () => observer.disconnect();
+        fetchData();
     }, []);
 
     return (
         <section className="News">
             <h2 className="News_title">Rapports Journalier:</h2>
             <div className="News_container">
-                {!isVisible && (
-                    <div className="news-facade">
-                        <p>Chargement des nouvelles...</p>
-                    </div>
-                )}
-                {Array.isArray(news) && news.map((article) => (
+                {news.map((article) => (
                     <div key={article.id} className="News_article">
-                        <img src={process.env.PUBLIC_URL + '/' + images[0].filename} alt={article.title} className="News_image" />
+                        <img src={process.env.PUBLIC_URL + '/' + images[0].filename} alt={article.title} className="News_image" loading="lazy"/>
                         <h3 className="News_article_title">{article.title}</h3>
                         <p className="News_article_summary">{article.summary}</p>
                         <a href={article.url} target="_blank" rel="noreferrer" className="News_article_link">Lire l'article</a>
