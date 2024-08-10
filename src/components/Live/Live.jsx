6e41f45137947deaf5d7fabb6ff4e8448863f229
zmt <zmt@MBP-zmt.local>
2024-08-10 10:26:38 +0200

    Fix broken link yt live

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -36,7 +36,7 @@ function Live() {
             {isVisible && (
                 <section className="Live_container">
                     <iframe className='FirstLive' src="https://www.youtube.com/embed/P9C25Un7xaM" title="YouTube video player live 1" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe className='TwoLive' src="https://www.youtube.com/embed/jPTD2gnZFUw" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe className='TwoLive' src="https://www.youtube.com/embed/FkP_Qxw5xns" title="YouTube video player live 2" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </section>
             )}
         </section>
